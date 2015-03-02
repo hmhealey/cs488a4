fd13eeb8d8189aa6f6c0ef19df8cc699880fa0e5
hmhealey <harrisonmhealey@gmail.com>
2015-03-01 19:02:30 -0500

    Initial work on getting lighting working. Something looks really off with it though.

--- src/a4.cpp
+++ src/a4.cpp
@@ -1,6 +1,7 @@
 #include "a4.hpp"
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 
 #include "algebra.hpp"
@@ -9,6 +10,8 @@
 #include "raycast.hpp"
 
 using namespace std;
+
+const bool DRAW_DEPTH = false;
 
 void render(SceneNode* root, const string& filename, int width, int height, 
             const Point3D& eye, const Vector3& view, const Vector3& up, double fov, 
@@ -43,36 +46,85 @@ void render(SceneNode* root, const string& filename, int width, int height,
 
     Matrix4 m = t4 * r3 * s2 * t1;
 
-    double dist[height][width];
-    double maxDist = -1;
+    if (!DRAW_DEPTH) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < height; x++) {
+                Point3D pk(x, y, 0);
+                Point3D pw = m * pk;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            Point3D pk(x, y, 0);
-            Point3D pw = m * pk;
+                Colour colour;
 
-            RaycastHit hit;
-            if (root->raycast(pw, (pw - eye).normalized(), hit)) {
-                dist[y][x] = (hit.point - eye).length();
-                maxDist = max(maxDist, dist[y][x]);
-            } else {
-                dist[y][x] = -1;
+                RaycastHit hit;
+                if (root->raycast(pw, (pw - eye).normalized(), hit)) {
+                    PhongMaterial* material = (PhongMaterial*) hit.material;
+
+                    // ambient light
+                    colour = material->ambient * ambient;
+
+                    for (auto i = lights.cbegin(); i != lights.cend(); i++) {
+                        Light* light = *i;
+
+                        Vector3 surfaceToLight = light->position - hit.point;
+                        Vector3 lightDirection = surfaceToLight.normalized();
+
+                        // calculate attenuation (well 1 / attenuation so we can later divide by it)
+                        double distance = surfaceToLight.length();
+                        double attenuation = light->falloff[0] + light->falloff[1] * distance + light->falloff[2] * distance * distance;
+
+                        // diffuse
+                        Colour diffuse = light->colour * material->diffuse * max(hit.normal.dot(lightDirection), 0.0) / attenuation;
+
+                        // specular
+                        Colour specular(0, 0, 0);
+                        if (hit.normal.dot(lightDirection) >= 0.0) {
+                            Vector3 r = -lightDirection.reflect(hit.normal);
+                            specular = light->colour * material->specular * pow(max(r.dot(view), 0.0), material->shininess) / attenuation;
+                        }
+
+                        colour += diffuse + specular;
+                    }
+                } else {
+                    colour = getBackground(x, y, width, height);
+                }
+
+                image(x, y, 0) = colour.r();
+                image(x, y, 1) = colour.g();
+                image(x, y, 2) = colour.b();
             }
         }
-    }
+    } else {
+        double dist[height][width];
+        double maxDist = -1;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            Colour colour;
-            if (dist[y][x] != -1) {
-                colour = Colour(dist[y][x] / maxDist);
-            } else {
-                colour = getBackground(x, y, width, height);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Point3D pk(x, y, 0);
+                Point3D pw = m * pk;
+
+                RaycastHit hit;
+                if (root->raycast(pw, (pw - eye).normalized(), hit)) {
+                    dist[y][x] = (hit.point - eye).length();
+                    maxDist = max(maxDist, dist[y][x]);
+                    if (hit.normal == Vector3::Zero) cerr << "zero normal!" << endl;
+                } else {
+                    dist[y][x] = -1;
+                }
             }
-            
-            image(x, y, 0) = colour.r();
-            image(x, y, 1) = colour.g();
-            image(x, y, 2) = colour.b();
+        }
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Colour colour;
+                if (dist[y][x] != -1) {
+                    colour = Colour(dist[y][x] / maxDist);
+                } else {
+                    colour = getBackground(x, y, width, height);
+                }
+                
+                image(x, y, 0) = colour.r();
+                image(x, y, 1) = colour.g();
+                image(x, y, 2) = colour.b();
+            }
         }
     }
 
